e6f88baf9d8e45b494b60f4893f61064801ae3b9
Natasha Buckham <Natasha.Buckham@softwire.com>
2024-11-06 16:37:50 +0000

    adds rule for 13 and refactors to add an applyRule function

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
 function fizzbuzz() {
-    for (let i = 1; i < 101; i++) {
+    for (let i = 1; i < 196; i++) {
+        
         let output: string[] = []
-        let rules = {3: "Fizz", 5: "Buzz", 7: "Bang", 11: "Bong"}
+        let rules = {3: "Fizz", 5: "Buzz", 7: "Bang", 11: "Bong", 13: "Fezz"}
 
-        const checkDivisor = (i: number, divisor: number) => {
-            if (i % divisor === 0) {
+        const checkDivisor = (i: number, divisor: number): boolean => {
+            return i % divisor === 0
+        }
+
+        const applyRule = (i: number, divisor: number) => {
+            if (checkDivisor(i, divisor)) {
                 output.push(rules[divisor])
             }
         }
 
-        checkDivisor(i, 11)
-
+        if (checkDivisor(i, 11)) {
+            applyRule(i, 13)
+            applyRule(i, 11)
+        }
+    
         if (output.length === 0) {
-            checkDivisor(i, 3)
-            checkDivisor(i, 5)
-            checkDivisor(i, 7)
+            applyRule(i, 3)
+            applyRule(i, 13)
+            applyRule(i, 5)
+            applyRule(i, 7)
         }
 
         if (output.length === 0) {
