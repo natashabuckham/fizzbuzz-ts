97fcf6012ef0c04a3ea5ebbc955925bdab6c1d6c
Natasha Buckham <Natasha.Buckham@softwire.com>
2024-11-06 15:49:40 +0000

    refactor and adds rule for multiples of 7

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 function fizzbuzz() {
     for (let i = 1; i < 101; i++) {
-        if (i % 15 === 0) {
-            console.log("FizzBuzz")
-        } else if (i % 3 === 0) {
-            console.log("Fizz")
-        } else if (i % 5 === 0) {
-            console.log("Buzz")
-        } else {
+        let output: string = "";
+
+        if (i % 3 === 0) {
+            output += "Fizz"
+        } 
+        
+        if (i % 5 === 0) {
+            output += "Buzz"
+        }
+
+        if (i % 7 === 0) {
+            output += "Bang"
+        }
+
+        if (output === "") {
             console.log(i)
+        } else {
+            console.log(output)
         }
     }    
 }
