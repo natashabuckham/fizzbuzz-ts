cea31e24f10c075db0dae3e6b0a93f97302b0d0f
Natasha Buckham <Natasha.Buckham@softwire.com>
2024-11-07 16:37:24 +0000

    Refactor to use less than or equal sign for the for loop

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 function fizzbuzz() {
-    for (let i = 1; i < 101; i++) {
+    for (let i = 1; i <= 100; i++) {
         if (i % 5 === 0 && i % 3 === 0) {
             console.log("FizzBuzz")
         } else if (i % 3 === 0) {
